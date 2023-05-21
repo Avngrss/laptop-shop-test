aaa8c84eebbfa1a4fedd976024ed75f63af213bd
Avngrss <97980284+Avngrss@users.noreply.github.com>
2023-05-21 16:00:54 +0300

    fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ onClickOpenCart }) {
     <div className="container d-flex justify-content-between bg-success align-items-center p-2 bg-opacity-10">
       <div className="headerRight">
         <div className="logo d-flex align-items-center">
-          <Link to={"/"}>
+          <Link to={""}>
             <div className="d-flex justify-content-between align-items-center gap-1">
               <img src="img/logo.svg" alt="logo" />
               <p className={style.logoText}>Laptopus.by</p>
@@ -19,7 +19,7 @@ function Header({ onClickOpenCart }) {
       </div>
       <div className="headerLeft d-flex justify-content-between align-items-center gap-3">
         <div className={style.likes}>
-          <Link to={"/favorite"}>
+          <Link to={"favorite"}>
             <img width="30px" height="30px" className="mr-5" src="img/like.svg" alt="like" />
             <span className={style.text}>Избранное</span>
           </Link>
